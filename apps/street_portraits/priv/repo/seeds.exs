33adeefb4d91f48b32faacf930f4607079a21d3a
Ryan Y <ryan.kho.yen@gmail.com>
2019-02-17 11:28:16 -0500

    Added more neighbors

--- apps/street_portraits/priv/repo/seeds.exs
+++ apps/street_portraits/priv/repo/seeds.exs
@@ -33,6 +33,33 @@ StreetPortraits.Repo.insert!(%StreetPortraits.SP.Neighbor{
       preferred_channel: "phone",
 })
 
+StreetPortraits.Repo.insert!(%StreetPortraits.SP.Neighbor{
+      address: "123 Main St., Dallas, TX, 75206",
+      approx_location: "Deep Ellum",
+      name: "Jack Black",
+      phone: "911-823-1000",
+      preferred_channel: "phone",
+})
+
+
+StreetPortraits.Repo.insert!(%StreetPortraits.SP.Neighbor{
+      address: "123 Main St., Dallas, TX, 75206",
+      approx_location: "Deep Ellum",
+      name: "Jane Doe",
+      phone: "911-823-1000",
+      preferred_channel: "phone",
+})
+
+
+StreetPortraits.Repo.insert!(%StreetPortraits.SP.Neighbor{
+      address: "123 Main St., Dallas, TX, 75206",
+      approx_location: "Deep Ellum",
+      name: "John Doe",
+      phone: "911-823-1000",
+      preferred_channel: "phone",
+})
+
+
 StreetPortraits.Repo.insert!(%StreetPortraits.SP.Portrait{
       body: "I became homeless when my marriage broke up. I had taken on a big mortgage and the interest rate went up. I became overstretched when my marriage ended. I had lost an income and then I also lost my job.
 I was falling further and further behind with my debt repayments. Though I didn’t know it at the time, I think I was having a mental meltdown. I wasn’t able to cope and began drinking too much. I quit my job because I wasn’t happy with the way things were being run at the organisation. 
@@ -74,4 +101,46 @@ Maecenas ac eros lorem. Vestibulum ultricies velit erat, eu scelerisque nunc mol
 Etiam vulputate tellus sit amet massa congue aliquam. Sed feugiat mattis ligula, eget aliquet neque tincidunt nec.",
       url: "https://images.unsplash.com/photo-1547524033-e1f60851349b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
       neighbor_id: 3
+})
+
+StreetPortraits.Repo.insert!(%StreetPortraits.SP.Portrait{
+      body: "I became homeless when my marriage broke up. I had taken on a big mortgage and the interest rate went up. I became overstretched when my marriage ended. I had lost an income and then I also lost my job.
+I was falling further and further behind with my debt repayments. Though I didn’t know it at the time, I think I was having a mental meltdown. I wasn’t able to cope and began drinking too much. I quit my job because I wasn’t happy with the way things were being run at the organisation. 
+I imagined I would quickly find another job, but it didn’t work out that way.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut purus ut nisi luctus efficitur. Aenean id nisi eu urna consectetur tempor. Aliquam erat volutpat. Ut lacinia ligula gravida magna iaculis vestibulum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum a imperdiet justo. Integer a ligula dui. Integer consequat lorem et rutrum elementum. Aenean tristique tellus ut ante hendrerit sodales.
+
+Aliquam erat risus, vestibulum eu vehicula non, aliquet nec tellus. Nunc ut molestie mi. Curabitur semper dolor quis magna tristique, ut rutrum massa ultricies. Vestibulum consequat quam purus, eu sodales massa tempus vel. Mauris dignissim turpis ut nibh tincidunt, tincidunt sodales risus porttitor. Nullam gravida dui eget nulla cursus aliquet. Sed non lacus a mi feugiat pharetra. Vestibulum magna orci, faucibus at est eu, finibus faucibus mi. Vestibulum ac porta mi, sodales finibus ex.
+
+Maecenas ac eros lorem. Vestibulum ultricies velit erat, eu scelerisque nunc molestie et. Nulla ac imperdiet nisl. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus egestas finibus elit quis maximus. Etiam quis tincidunt orci. Aliquam mollis, diam vel fermentum viverra, dolor orci sollicitudin lorem, in sodales eros quam id mauris. Duis tincidunt tempus orci nec lacinia. Suspendisse id dui ac mi tempor condimentum. Sed sit amet odio urna. Praesent ut euismod ante. Etiam lacinia tellus urna, id porta sapien auctor nec. 
+Etiam vulputate tellus sit amet massa congue aliquam. Sed feugiat mattis ligula, eget aliquet neque tincidunt nec.",
+      url: "https://images.unsplash.com/photo-1518398046578-8cca57782e17?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+      neighbor_id: 4
+})
+
+StreetPortraits.Repo.insert!(%StreetPortraits.SP.Portrait{
+      body: "I became homeless when my marriage broke up. I had taken on a big mortgage and the interest rate went up. I became overstretched when my marriage ended. I had lost an income and then I also lost my job.
+I was falling further and further behind with my debt repayments. Though I didn’t know it at the time, I think I was having a mental meltdown. I wasn’t able to cope and began drinking too much. I quit my job because I wasn’t happy with the way things were being run at the organisation. 
+I imagined I would quickly find another job, but it didn’t work out that way.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut purus ut nisi luctus efficitur. Aenean id nisi eu urna consectetur tempor. Aliquam erat volutpat. Ut lacinia ligula gravida magna iaculis vestibulum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum a imperdiet justo. Integer a ligula dui. Integer consequat lorem et rutrum elementum. Aenean tristique tellus ut ante hendrerit sodales.
+
+Aliquam erat risus, vestibulum eu vehicula non, aliquet nec tellus. Nunc ut molestie mi. Curabitur semper dolor quis magna tristique, ut rutrum massa ultricies. Vestibulum consequat quam purus, eu sodales massa tempus vel. Mauris dignissim turpis ut nibh tincidunt, tincidunt sodales risus porttitor. Nullam gravida dui eget nulla cursus aliquet. Sed non lacus a mi feugiat pharetra. Vestibulum magna orci, faucibus at est eu, finibus faucibus mi. Vestibulum ac porta mi, sodales finibus ex.
+
+Maecenas ac eros lorem. Vestibulum ultricies velit erat, eu scelerisque nunc molestie et. Nulla ac imperdiet nisl. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus egestas finibus elit quis maximus. Etiam quis tincidunt orci. Aliquam mollis, diam vel fermentum viverra, dolor orci sollicitudin lorem, in sodales eros quam id mauris. Duis tincidunt tempus orci nec lacinia. Suspendisse id dui ac mi tempor condimentum. Sed sit amet odio urna. Praesent ut euismod ante. Etiam lacinia tellus urna, id porta sapien auctor nec. 
+Etiam vulputate tellus sit amet massa congue aliquam. Sed feugiat mattis ligula, eget aliquet neque tincidunt nec.",
+      url: "https://images.unsplash.com/photo-1518398046578-8cca57782e17?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+      neighbor_id: 5
+})
+
+StreetPortraits.Repo.insert!(%StreetPortraits.SP.Portrait{
+      body: "I became homeless when my marriage broke up. I had taken on a big mortgage and the interest rate went up. I became overstretched when my marriage ended. I had lost an income and then I also lost my job.
+I was falling further and further behind with my debt repayments. Though I didn’t know it at the time, I think I was having a mental meltdown. I wasn’t able to cope and began drinking too much. I quit my job because I wasn’t happy with the way things were being run at the organisation. 
+I imagined I would quickly find another job, but it didn’t work out that way.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut purus ut nisi luctus efficitur. Aenean id nisi eu urna consectetur tempor. Aliquam erat volutpat. Ut lacinia ligula gravida magna iaculis vestibulum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum a imperdiet justo. Integer a ligula dui. Integer consequat lorem et rutrum elementum. Aenean tristique tellus ut ante hendrerit sodales.
+
+Aliquam erat risus, vestibulum eu vehicula non, aliquet nec tellus. Nunc ut molestie mi. Curabitur semper dolor quis magna tristique, ut rutrum massa ultricies. Vestibulum consequat quam purus, eu sodales massa tempus vel. Mauris dignissim turpis ut nibh tincidunt, tincidunt sodales risus porttitor. Nullam gravida dui eget nulla cursus aliquet. Sed non lacus a mi feugiat pharetra. Vestibulum magna orci, faucibus at est eu, finibus faucibus mi. Vestibulum ac porta mi, sodales finibus ex.
+
+Maecenas ac eros lorem. Vestibulum ultricies velit erat, eu scelerisque nunc molestie et. Nulla ac imperdiet nisl. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus egestas finibus elit quis maximus. Etiam quis tincidunt orci. Aliquam mollis, diam vel fermentum viverra, dolor orci sollicitudin lorem, in sodales eros quam id mauris. Duis tincidunt tempus orci nec lacinia. Suspendisse id dui ac mi tempor condimentum. Sed sit amet odio urna. Praesent ut euismod ante. Etiam lacinia tellus urna, id porta sapien auctor nec. 
+Etiam vulputate tellus sit amet massa congue aliquam. Sed feugiat mattis ligula, eget aliquet neque tincidunt nec.",
+      url: "https://images.unsplash.com/photo-1518398046578-8cca57782e17?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
+      neighbor_id: 6
 })
